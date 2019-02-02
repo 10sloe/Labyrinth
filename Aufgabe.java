6a261527e38d4158188c92885d0e5d40fa4aaa09
10sloe <43334289+10sloe@users.noreply.github.com>
2019-02-02 19:37:31 +0100

    Add files via upload

--- Aufgabe.java
+++ Aufgabe.java
@@ -9,13 +9,41 @@ class Aufgabe
     // Konstruktor
     Aufgabe()
     {
-        welt = new Welt("laby1.kdw");
+        welt = new Welt("laby2.kdw");
         robo = new Roboter(welt);
     }
 
     void los()
     {
-       
-    }   
+        // Gehe bis zur ersten Wand
+        while(!robo.IstZiegel())
+        {
+            robo.Schritt();
+        }
+        robo.RechtsDrehen();
+        while(!robo.IstWand())
+        {
+            if(!robo.IstZiegel())
+            {
+                robo.Schritt();
+                if(!robo.IstZiegelLinks())
+                {
+                    robo.LinksDrehen();
+                }
+            }
+            else
+            {
+                if(robo.IstZiegelLinks())
+                {
+                    robo.RechtsDrehen();
+                }
+                else
+                {
+                    robo.LinksDrehen();
+                }
+            }
+            
+        }
 
+    }
 }
